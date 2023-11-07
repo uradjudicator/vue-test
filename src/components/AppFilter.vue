a0412c12f1d8f37ef99e7fdff08e34ff3e0d4539
uradjudicator <prind0241@gmail.com>
2023-11-07 16:11:11 +0500

    feat:completed Appfilter section

--- src/components/AppFilter.vue
+++ src/components/AppFilter.vue
@@ -1,6 +1,14 @@
 <template>
-    <div>
-        <h1>AppFilter</h1>
+    <div class="btn-group">
+        <button class="btn btn-outline-dark" type="button">
+            Barcha Kinolar
+        </button>
+        <button class="btn btn-outline-dark" type="button">
+            Mashhur Kinolar
+        </button>
+        <button class="btn btn-outline-dark" type="button">
+            Eng Ko'p ko'rilgan Kinolar
+        </button>
     </div>
 </template>
 <script>
--- src/components/searchPanel.vue
+++ src/components/searchPanel.vue
@@ -10,6 +10,7 @@ export default {
 <style scoped>
 .search-input {
     margin-top: 2rem;
+    margin-bottom: 1rem;
     padding: 1.5rem;
     background-color: #fcfaf5;
     border-radius: 4px;
