39d4cf0dc1a60165c37cc3a468ff2d978f5eb8b0
uradjudicator <prind0241@gmail.com>
2023-11-07 16:06:08 +0500

    feat:completed search-panel section

--- src/components/searchPanel.vue
+++ src/components/searchPanel.vue
@@ -1,14 +1,18 @@
 <template>
-    <div>
-        search panel
-    </div>
+    <input type="text" class="form-control search-input" placeholder="Kino qidirish...">
 </template>
 
 <script>
 export default {
-    
+
 }
 </script>
 <style scoped>
-    
+.search-input {
+    margin-top: 2rem;
+    padding: 1.5rem;
+    background-color: #fcfaf5;
+    border-radius: 4px;
+    box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.15);
+}
 </style>
