c222367d5d37ff7bdc47c87f94b285b591cd5b9f
uradjudicator <prind0241@gmail.com>
2023-11-08 13:21:44 +0500

    feat:completed layout and need to add functional

--- src/components/movie-list-item/movie-list-item.vue
+++ src/components/movie-list-item/movie-list-item.vue
@@ -1,15 +1,99 @@
 <template >
-    <div>
-        <li>John Wick 4</li>
-      <li>Fast and Furious X</li>
-      <li>Hobs & Shaw</li>
-    </div>
-</template>
+   
+        <li class="list-group-item d-flex justify-content-between favourite ">
+          <span class="list-group-item-label">John Wick 4</span>
+          <input type="number" inputmode="numeric" class="list-group-item-input" defaultValue="529">
+
+          <div class="d-flex justify-content-center align-items-center">
+
+            <button type="button" class="btn-cookie btn-sm">
+              <i class="fa-solid fa-cookie"></i>
+            </button>
+            
+            <button type="button" class="btn-trash btn-sm">
+              <i class="fa-solid fa-trash"></i>
+            </button>
+            
+            <i class="fa-solid fa-star"></i>
+          </div>
+
+        </li>
+    </template>
 <script>
 export default {
-    
+  
 }
 </script>
 <style scoped>
+    .list-group-item {
+      padding: 15px 20px;
+      border: none;
+      border-bottom: 1px solid #3d5a80;
+    }
+    .list-group-item:last-child{
+      border-bottom: none;
+    }
+    .list-group-item span {
+      line-height: 35px;
+      font-size: 20px;
+      cursor: pointer;
+      width: 550px;
+    }
+    .list-group-item input {
+      line-height: 35px;
+      font-size: 20px;
+      text-align: center;
+      border: 0;
+      outline: none;
+    }
+    .list-group-item button{
+      width: 35px;
+      height: 35px;
+      margin: 3px;
+      font-size: 17px;
+      border: none;
+      cursor: pointer;
+    }
+
+    .list-group-item .btn-cookie{
+      color: #e09f3e;
+    }
+    .list-group-item .btn-trash{
+      color: #e5383b;
+    }
+
+    .list-group-item .fa-star{
+      width: 35px;
+      height: 35px;
+      text-align: center;
+      line-height: 35px;
+      font-size: 16px;
+      color: #ffd700;
+      transition: 0.3 all;
+      transform: translateX(30px);
+      opacity: 0;
+    }
     
-</style>
+    .list-group-item.like .fa-star{
+      opacity: 1;
+      transform: translateX(0);
+    } 
+
+    .list-group-item.favourite .list-group-item-input,
+    .list-group-item.favourite .list-group-item-label{
+      color: #e09f3e;
+    }
+    </style>
+
+
+
+
+
+
+
+
+
+
+
+    <!-- <li>Fast and Furious X</li>
+    <li>Hobs & Shaw</li> -->
