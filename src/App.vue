6d4229719d52d2e08f736916709e90ce810288d0
uradjudicator <prind0241@gmail.com>
2023-11-08 22:22:30 +0500

    feat:working on adding functionals

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,75 @@
 <template >
   <div class="app font-monospace">
     <div class="content">
-      <AppInfo :allMoviesCount="movies.length" :favouriteMoviesCount="movies.filter(c => c.favourite).length"/>
+      <AppInfo :allMoviesCount="movies.length" :favouriteMoviesCount="movies.filter(c => c.favourite).length" />
       <div class="search-panel">
         <searchPanel />
         <AppFilter />
       </div>
-      <MovieList :movies="movies"/>
+      <MovieList :movies="movies" />
       <movieAddForm />
     </div>
   </div>
 </template>
 <script>
-  import AppInfo from "@/components/app-info.vue"
-  import searchPanel from "./components/searchPanel.vue";
-  import AppFilter from "./components/AppFilter.vue"
-  import MovieList from "./components/movieList.vue"
-  import movieAddForm from "./components/movieAddForm.vue";
+import AppInfo from "@/components/app-info.vue"
+import searchPanel from "./components/searchPanel.vue";
+import AppFilter from "./components/AppFilter.vue"
+import MovieList from "./components/movieList.vue"
+import movieAddForm from "./components/movieAddForm.vue";
 export default {
-  components:{
+  components: {
     AppInfo,
     searchPanel,
     AppFilter,
     MovieList,
     movieAddForm
- },
- data() {
-        return {
-            movies: [
-                {
-                    name: 'John Wick 4', 
-                    viewers:'458',
-                    favourite:false,
-                    like: true
-                },
-               {
-                    name: 'Fast and Furious X', 
-                    viewers:'633',
-                    favourite:false,
-                    like: false
-                },
-                {
-                    name: 'Hobs & Shaw 2', 
-                    viewers:'571',
-                    favourite:true,
-                    like: false
-                },
-               
-            ]
-        }
+  },
+  data() {
+    return {
+      movies: [
+        {
+          name: 'John Wick 4',
+          viewers: '458',
+          favourite: false,
+          like: true
+        },
+        {
+          name: 'Fast and Furious X',
+          viewers: '633',
+          favourite: false,
+          like: false
+        },
+        {
+          name: 'Hobs & Shaw 2',
+          viewers: '571',
+          favourite: true,
+          like: false
+        },
+
+      ]
     }
+  }
 }
 </script>
 <style>
-  .app
-  {
-    height: 100%;
-  }
-  .content{
-    width: 1000px;
-    min-height: 700px;
-    background-color: #333;
-    margin: 0 auto;
-    padding: 5rem 0;
-  }
+.app {
+  height: 100%;
+}
 
-  .search-panel{
-    margin-top: 2rem;
-    padding: 1.5rem;
-    background-color: #fcfaf5;
-    border-radius: 4px;
-    box-shadow: 15px 15px 15px rgba(0,0,0,0.15);
-  }
+.content {
+  width: 1000px;
+  min-height: 700px;
+  background-color: #333;
+  margin: 0 auto;
+  padding: 5rem 0;
+}
+
+.search-panel {
+  margin-top: 2rem;
+  padding: 1.5rem;
+  background-color: #fcfaf5;
+  border-radius: 4px;
+  box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.15);
+}
 </style>
--- src/components/movieAddForm.vue
+++ src/components/movieAddForm.vue
@@ -1,26 +1,47 @@
 <template>
     <div class="movie-add-form">
         <h3>Yangi kino qo'shish </h3>
-        <form class="add-form d-flex">
-            <input type="text" class="form-control new-movie-label" placeholder="Qanday kino?">
-            <input type="text" class="form-control new-movie-label" placeholder="Necha marotaba ko'rilgan?">
-            <button class="btn btn-outline-dark" type="button">Qo'shish</button>
+        <form class="add-form d-flex" @submit.prevent>
+            <input type="text" class="form-control new-movie-label" placeholder="Qanday kino?" :value="name"
+                @:input="name = $event.target.value">
+
+            <input type="number" class="form-control new-movie-label" placeholder="Necha marotaba ko'rilgan?"
+                :value="viewers" @:input="viewers = $event.target.value">
+
+            <button class="btn btn-outline-dark" type="button" @click="addMovie">Qo'shish</button>
         </form>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            name: '',
+            viewers: '',
+        }
+    },
+    methods: {
+        addMovie() {
+            const newMovie = {
+                name: this.name,
+                viewers: this.viewers,
+                favourite: false,
+                like: false
+            }
+            console.log(newMovie);
+        }
+    }
 
 }
 </script>
 
 <style scoped>
-.movie-add-form{
+.movie-add-form {
     margin-top: 2rem;
     padding: 1.5rem;
     background-color: #fcfaf5;
     border-radius: 4px;
-    box-shadow: 15px 15px 15px rgba(0,0,0,0.15);
+    box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.15);
 }
 </style>
